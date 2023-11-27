98bcc73e9191cd6fc1aa66a3b00cf2b46acf5983
Xarri Jorge <kodinglife@gmail.com>
2023-11-27 00:48:38 +0000

    Minor updates to userModel

--- models/userModel.js
+++ models/userModel.js
@@ -12,7 +12,6 @@ const userSchema = new mongoose.Schema({
     },
     maidenName: {
         type: String,
-        required: true,
     },
     age: {
         type: Number,
@@ -50,7 +49,6 @@ const userSchema = new mongoose.Schema({
     },
     university: {
         type: String,
-        required: true,
     },
     bank: {
         type: String,
