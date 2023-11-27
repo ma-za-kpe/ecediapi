68ee341f8e4074f89979e8e9852985b265f2063e
Xarri Jorge <kodinglife@gmail.com>
2023-11-27 00:45:14 +0000

    POST and PUT routes for userModel

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,25 @@ router.get('/', (req, res, next) => {
     .catch(error => next(error));
 });
 
+router.post('/', (req, res, next) => {
+  const user = new User(JSON.parse(req.body.user));
+
+  user
+    .save()
+    .then(savedUser => {
+      res.json(savedUser);
+    })
+    .catch(error => next(error));
+});
+
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  User.findByIdAndUpdate(id, { firstName: 'Jane' }, { new: true })
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(error => next(error));
+});
+
 export default router;
