3fd23bd22c37eaf1c49e9bc0a4f48f3049da1ca5
Xarri Jorge <kodinglife@gmail.com>
2023-11-27 00:47:24 +0000

    Added category to userModel

--- models/userModel.js
+++ models/userModel.js
@@ -36,6 +36,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    category: {
+        type: String,
+        required: true,
+    },
     image: {
         type: String,
         required: true,
