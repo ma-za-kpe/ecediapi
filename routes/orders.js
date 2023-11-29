7f32e1ad288712c26b224010c7452624d187fba4
Xarri Jorge <kodinglife@gmail.com>
2023-11-29 13:16:56 +0000

    Renamed orderModel

---
+++ routes/orders.js
@@ -0,0 +1,45 @@
+import express from "express";
+import Order from "../models/orderModel.js";
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Order.find({})
+        .then(order => {
+            res.json(order);
+        })
+        .catch(error => next(error));
+});
+
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    Orders.findById(id)
+        .then(order => {
+            res.json(order);
+        })
+        .catch(error => next(error));
+});
+
+router.post('/', (req, res, next) => {
+    const order = new order(JSON.parse(req.body.order));
+
+    order
+        .save()
+        .then(savedOrder => {
+            res.json(savedOrder);
+        })
+        .catch(error => next(error));
+});
+
+router.put('/:id', (req, res, next) => {
+    const { id } = req.params;
+    const order = req.body.order;
+
+    order.findByIdAndUpdate(id, order, { new: true })
+        .then(updatedOrder => {
+            res.json(updatedOrder);
+        })
+        .catch(error => next(error));
+});
+
+export default router;
